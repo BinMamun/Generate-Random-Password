68f7fc3be8ea9c588a4f2e576b966f9907b11fce
Abdullah Bin Mamun <binbean44@gmail.com>
2023-11-20 01:58:08 +0600

    Gerenrate Random password

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const randomPassword = document.querySelector(".js-password");
+const generateButton = document.querySelector(".js-generate-password-btn");
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%&*/.";
+
+const allChars = upperCase + lowerCase + numbers + symbols;
+
+
+generateButton.addEventListener("click", () => {
+  passordBox();
+})
+
+function passordBox() {
+  let password = "";
+  let length = 8;
+
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password += symbols[Math.floor(Math.random() * symbols.length)];
+
+  while (password.length < length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)]
+  }
+
+  randomPassword.value = password;
+}
